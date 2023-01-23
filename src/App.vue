e5f6e0a7d05f651eaa08eae3d3bc23be8812fc0c
Kostya Rybak <62746770+K-Rybak@users.noreply.github.com>
2023-01-23 20:57:31 +0600

    added event on checkbox

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@ export default {
   data() {
     return {
       todos: [
-        {id: 1, title: 'Купить хлеб', completed: true},
+        {id: 1, title: 'Купить хлеб', completed: false},
         {id: 2, title: 'Купить масло', completed: false},
         {id: 3, title: 'Купить молоко', completed: false}
       ]
