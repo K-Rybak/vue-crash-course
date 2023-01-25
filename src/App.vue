650960fae321acee4009c7beeb317200c675fe3a
Kostya Rybak <62746770+K-Rybak@users.noreply.github.com>
2023-01-25 22:23:50 +0600

    added working with server

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,8 @@
 <template>
   <h1>TodoList application</h1>
-  <AddForm 
-    @add-todo="addTodo"
-  />
+  <AddForm @add-todo="addTodo" />
   <hr>
-  <TodoList
-    v-bind:todos="todos" 
-    @remove-item="removeTodo"
-  />
+  <TodoList v-bind:todos="todos" @remove-item="removeTodo" />
 </template>
 
 <script>
@@ -18,19 +13,22 @@ export default {
   name: 'App',
   data() {
     return {
-      todos: [
-        {id: 1, title: 'Купить хлеб', completed: false},
-        {id: 2, title: 'Купить масло', completed: false},
-        {id: 3, title: 'Купить молоко', completed: false}
-      ]
+      todos: []
     }
   },
   components: {
     TodoList,
     AddForm
   },
+  mounted() {
+    fetch('https://jsonplaceholder.typicode.com/todos?_limit=3')
+      .then(response => response.json())
+      .then(json => {
+        this.todos = json
+      })
+  },
   methods: {
-    removeTodo(id){
+    removeTodo(id) {
       this.todos = this.todos.filter(x => x.id !== id)
     },
     addTodo(todo) {
@@ -41,12 +39,12 @@ export default {
 </script>
 
 <style>
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-  }
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
